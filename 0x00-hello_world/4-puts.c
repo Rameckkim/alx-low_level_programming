e9e73ecee34371bb0818a01c2d14f53b7148e910
Rameck Kimani <rameckkim@gmail.com>
2023-07-13 13:41:42 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - print a string
  *
- *Return: always 0
- */ 
+ * Return: always 0
+ */
+
 int main(void)
-{       puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
