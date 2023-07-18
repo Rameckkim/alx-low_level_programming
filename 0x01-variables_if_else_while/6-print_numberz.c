8b2e3525e3189312237108fda9069c91282621d3
Rameck Kimani <rameckkim@gmail.com>
2023-07-18 12:56:39 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
