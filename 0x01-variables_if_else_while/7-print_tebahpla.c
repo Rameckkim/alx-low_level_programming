15c5af52d0533ba7061420db58645afcd66d2411
Rameck Kimani <rameckkim@gmail.com>
2023-07-18 13:23:32 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
