ce7fb26f749deafa35a2da26d6af9a6b33665b5d
Rameck Kimani <rameckkim@gmail.com>
2023-07-21 09:32:03 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+i#include "main.h"
+
+/**
+ * _isupper - looks for an uppercase character
+ * @c: character to be evaluated
+ * Return: 1 if uppercase and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
