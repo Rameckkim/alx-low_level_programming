098219c9794fbb6d5dd12887b38cfbf9a4370332
Rameck Kimani <rameckkim@gmail.com>
2023-07-21 10:48:35 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of two intergers
+ * @a:interger a
+ * @b:interger b
+ * Return:results of multiplacation of intergers
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+
+	result(a * b);
+	return (result);
+}
+
