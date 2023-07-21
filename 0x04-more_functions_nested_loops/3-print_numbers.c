3874356bb334d2ff64b81f9d00cdb1b2a61eaa03
Rameck Kimani <rameckkim@gmail.com>
2023-07-21 14:34:48 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers- prints number 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
