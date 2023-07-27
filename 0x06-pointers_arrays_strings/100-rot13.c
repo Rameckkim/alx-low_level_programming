916459186ab20e3ba388ee734d6a0d0fd98865bb
Rameck Kimani <rameckkim@gmail.com>
2023-07-27 13:23:43 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,18 +6,17 @@
  * @input: The input string to be encoded.
  *
  * Return: A pointer to the dynamically allocated encoded string.
- *         If the input string is NULL or memory fails, it returns NULL.
+ *         If the input string is NULL or memory allocation fails, it returns NULL.
  */
 char *rot13(char *input)
 {
-	if (input == NULL)
-		return (NULL);
+	if (input == NUL)L
+		return (NULL); /* Return NULL if the input string is NULL */
 
+	/* Allocate memory for the encoded string, adding 1 for the null-terminator */
 	char *encoded = (char *)malloc(strlen(input) + 1);
-
 	if (encoded == NULL)
-
-		return (NULL);
+		return (NULL); /* Return NULL if memory allocation fails */
 
 	int i = 0;
 	int len = strlen(input);
@@ -26,23 +25,24 @@ char *rot13(char *input)
 	{
 		char ch = input[i];
 
+		/* Check if the character is an uppercase or lowercase letter */
 		if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
 		{
-
+			/* Apply ROT13 encoding on the character */
 			if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'm'))
-				encoded[i] = ch + 13;
+				encoded[i] = ch + 13; /* Shift forward by 13 positions */
 			else
-				encoded[i] = ch - 13;
+				encoded[i] = ch - 13; /* Shift backward by 13 positions */
 		}
 		else
 		{
-			encoded[i] = ch;
+			encoded[i] = ch; /* Non-letter characters remain unchanged */
 		}
 
 		i++;
 	}
 
-	encoded[i] = '\0';
+	encoded[i] = '\0'; /* Null-terminate the encoded string */
 	return (encoded);
 }
 
