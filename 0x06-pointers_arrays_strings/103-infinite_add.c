d93860cc969dd425e30a1f1d0ce712d796aaa34e
Rameck Kimani <rameckkim@gmail.com>
2023-07-28 13:30:30 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,50 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
+#include <ctype.h>
 
 /**
- * reverse_string - Reverses a given string in-place.
- * @str: The string to be reversed.
+ * print_buffer - Prints the content of a buffer.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to print.
  */
-void reverse_string(char *str)
+void print_buffer(char *b, int size)
 {
-	int start = 0;
-	int end = strlen(str) - 1;
+	int i, j;
+	char hex[2];
 
-	while (start < end)
+	for (i = 0; i < size; i += 10)
 	{
-		char temp = str[start];
-		str[start] = str[end];
-		str[end] = temp;
+		/* Print the starting position of the line in hexadecimal */
+		for (j = 7; j >= 0; j--)
+			hex[j] = "0123456789abcdef"[i >> (4 * (7 - j)) & 0xf];
+		for (j = 0; j < 8; j++)
+			putchar(hex[j]);
+		putchar(':');
+		putchar(' ');
 
-		start++;
-		end--;
-	}
-}
+		/* Print hexadecimal content and corresponding characters */
+		for (j = 0; j < 10; j++)
+		{
+			if (j + i < size)
+			{
+				hex[0] = "0123456789abcdef"[b[i + j] >> 4 & 0xf];
+				hex[1] = "0123456789abcdef"[b[i + j] & 0xf];
+				putchar(hex[0]);
+				putchar(hex[1]);
+			}
+			else
+				putchar(' '), putchar(' ');
 
-/**
- * infinite_add - Adds two numbers and stores the result in a buffer.
- * @n1: The first number as a string.
- * @n2: The second number as a string.
- * @r: The buffer to store the result.
- * @size_r: The size of the buffer.
- *
- * Return: A pointer to the result (in the buffer r).
- *         If the result cannot be stored in r, the function returns 0.
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int carry = 0, sum = 0, i = 0;
+			if (j % 2 != 0)
+				putchar(' ');
 
-	/* Ensure the buffer size is sufficient to hold the result */
-	if (size_r <= len1 || size_r <= len2)
-		return (0);
-
-	/* Start adding digits from the least significant digits */
-	len1--;
-	len2--;
-
-	/* Loop through the numbers and perform addition */
-	while (len1 >= 0 || len2 >= 0 || carry)
-	{
-		/* Calculate the sum of digits and carry */
-		sum = carry;
-		if (len1 >= 0)
-			sum += n1[len1] - '0';
-		if (len2 >= 0)
-			sum += n2[len2] - '0';
-
-		/* Update carry for the next iteration */
-		carry = sum / 10;
-
-		/* Store the current digit in the result buffer */
-		r[i] = '0' + (sum % 10);
-
-		/* Move to the next position in the buffer and numbers */
-		i++;
-		len1--;
-		len2--;
+			/* Print the character representation of the byte */
+			if (j + i < size)
+				putchar(isprint(b[i + j]) ? b[i + j] : '.');
+		}
+		putchar('\n');
 	}
 
-	/* Null-terminate the result string */
-	r[i] = '\0';
-
-	/* Reverse the result string to get the correct order */
-	reverse_string(r);
-
-	return (r);
+	if (size <= 0)
+		putchar('\n');
 }
 
