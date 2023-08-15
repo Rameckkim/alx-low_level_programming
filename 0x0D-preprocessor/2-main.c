0e40ed36443759254e8a6079dfd349bafeddbee9
Rameck Kimani <rameckkim@gmail.com>
2023-08-14 19:56:23 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program.
- *
- * Description: Prints the name of the current source file.
- *
- * Return: Always 0
+ * main - prints the file name
+ * Return: 0
  */
+
 int main(void)
 {
-	printf("Compiled from file: %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
-}
-
+}i
